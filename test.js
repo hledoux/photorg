e011d2cd578b4247d0a2659bc56723107eda9397
LEDOUX Herve <herve.ledoux@gmail.com>
2018-01-21 16:41:09 +0100

    -

--- test.js
+++ test.js
@@ -2,8 +2,19 @@
 
 "use strict";
 
-const { f_is_false } = require("./string");
-const { f_console_info, f_console_inspect } = require("./console");
+const {
+  f_is_false
+} = require("./string");
+
+
+const {
+  f_console_alert,
+  f_console_error,
+  f_console_fatal,
+  f_console_info,
+  f_console_inspect
+} = require("./console");
+
 
 f_console_info("hello");
 
@@ -13,3 +24,9 @@ f_console_inspect({
   "f_is_false(true)" : f_is_false("true"),
   "f_is_false(false)" : f_is_false("false"),
 });
+
+
+f_console_info('this is an info');
+f_console_alert('this is an alert');
+f_console_error('this is an error');
+f_console_fatal('this is a fatal');
